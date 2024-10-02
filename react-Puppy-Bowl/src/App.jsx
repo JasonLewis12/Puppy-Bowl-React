c74f8ee77b189737aeba7f6407c4db8f5867201c
JasonLewis12 <Lewisjason64@yahoo.com>
2024-10-01 23:33:40 -0400

    added get request

--- react-Puppy-Bowl/src/App.jsx
+++ react-Puppy-Bowl/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {routes, route, link} from `react-router-dom`
+import { getAllPuppys } from './API'
 import './App.css'
 
 function App() {
   
   return (
     <>
-      
+      <div>
+
+      </div>
     </>
   )
 }
