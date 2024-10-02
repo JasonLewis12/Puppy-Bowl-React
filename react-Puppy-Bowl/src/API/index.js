bd7ac455f65124dfd93de817f1bcb47007050550
JasonLewis12 <Lewisjason64@yahoo.com>
2024-10-01 23:51:27 -0400

    added logic to request from API

--- react-Puppy-Bowl/src/API/index.js
+++ react-Puppy-Bowl/src/API/index.js
@@ -3,13 +3,51 @@ const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api${chort}`; //creating A
 
 //create get all reqest
 export async function getAllPuppys() {
-  const response = await fetch(`${API_URL}/players`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${API_URL}/players`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    "there was an error in your get request", error;
+  }
 }
 
 // create get single request
-
+export async function getOnePuppy(id) {
+  try {
+    const reponse = await fetch(`${API_URL}/players/${id}`);
+    const data = await reponse.json();
+    return data;
+  } catch (error) {
+    "there was an error in your get single request", error;
+  }
+}
 //create a post request
-
+export async function createPuppy(name, breed, imageUrl, teamId) {
+  try {
+    const reponse = fetch(`${API_URL}/players`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        breed,
+        imageUrl,
+        teamId,
+      }),
+    });
+    const puppy = await reponse.json();
+    return puppy;
+  } catch (error) {
+    "there was an error is your post request", error;
+  }
+}
 // create delete request
+export async function deletePuppy(id) {
+  try {
+    const reponse = await fetch(`${API_URL}/players/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    "there was an error in your delete request", error;
+  }
+}
